f621f020c29a2875ec14dd584de3dc1fbe39ebf0
Lucian <livanov@uoc.edu>
2021-04-05 23:19:49 +0200

    Nuevo componente song, sin ningun css. Nuevo objeto song, incluyendo datos de ejemplo. He añadido las dependencias de Bootstrap en angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {SongDetailsComponent} from './song-details/song-details.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        SongDetailsComponent
-    ],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule
--- src/app/songs/songs.js
+++
@@ -1,9 +0,0 @@
-import {Song} from './song';
-
-export const SONGS: Song[] =[
-  {id: 1, Title: "Como el Agua (Remix)", Explicit: false, Author: "Ana Mena, Omar Montes", Composer: "Ana Mena", Record: "Como el Agua (Remix)", Release: new Date(13, 12, 2019), Style: "Reggaeton",File: "comoelaguaremix.mp3"},
-  {id: 2, Title: "El Efecto", Explicit: false, Author: "Myke Towers", Composer: "Myke Towers", Record: "Easy Money Baby", Release: new Date(15, 3, 2019), Style: "Reggaeton", File: "elefecto.mp3"},
-  {id: 3, Title: "CHANGE", Explicit: true, Author: "Lit Killah", Composer: "Lit Killah", Record: "CHANGE", Release: new Date(18, 2, 2021), Style: "Trap", File: "change.mp3"},
-  {id: 4, Title: "El Pozo", Explicit: false, Author: "IZAL", Composer: "IZAL", Record: "Autoterapia", Release: new Date(9, 2, 2018), Style: "Indie", File: "elpozo.mp3"},
-  {id: 5, Title: "Chin Chin", Explicit: true, Author: "Aiman JR", Composer: "Aiman Nassar", Record: "Chin Chin", Release: new Date(2021, 1, 6), Style: "Drill", File: "chinchin.mp3"},
-]
